35ab1128d4d8a6c76c37ba26ac64d8147352b592
Iris <eyelessiris@hotmail.com>
2025-07-16 20:16:14 +0900

    1.1.1 Release

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const token = process.env.TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -26,33 +25,17 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`${interaction.commandName}와(과) 일치하는 명령어가 없습니다.`);
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.follwUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral })
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-        }
-    }
-    console.log(interaction);
-});
-
-
-
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Operating as '${readyClient.user.tag}. Ready!`);
-});
+}
 
 client.login(token);
