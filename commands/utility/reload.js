f9df6ff332b16f07c3a0572b14f9e79c151b31b6
Iris <eyelessiris@hotmail.com>
2025-07-17 20:40:00 +0900

    Experimental 1.2.6

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -46,10 +46,10 @@ module.exports = {
                 }
             }
 
-            await interaction.reply({ content: `불러오기 성공`, ephemeral: true });
+            await interaction.reply({ content: `명령어 다시 불러오기 성공`, ephemeral: true });
         } catch (error) {
             console.error('Failed to reload commands:', error);
-            await interaction.reply({ content: `불러오기 실패:\n\`${error.message}\``, ephemeral: true });
+            await interaction.reply({ content: `명령어 다시 불러오기 실패:\n\`${error.message}\``, ephemeral: true });
         }
     },
 };
